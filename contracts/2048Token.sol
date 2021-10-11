1cd7c44fcaf6e319296da42b324f55b576004b4d
Kevin Browne <kjbrowne28@gmail.com>
2021-10-11 17:04:42 -0400

    Connected token reward to Game changes

--- contracts/2048Token.sol
+++ contracts/2048Token.sol
@@ -2,8 +2,9 @@ pragma solidity ^0.8.0;
 
 
 import "../client/node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../client/node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
-contract TZFEToken is ERC20{
+contract TZFEToken is ERC20, Ownable{
     constructor(uint256 amount) ERC20("TZFE", "TFE") {
         require(amount > 0);
         _mint(msg.sender, amount);
