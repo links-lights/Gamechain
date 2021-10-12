086d67d856d25db26404a161883765cef878635d
Ben Ye <oceanblueye@gmail.com>
2021-10-11 21:31:39 -0400

    added reward-method-test

--- test/tokenTest.js
+++ test/tokenTest.js
@@ -1,3 +1,4 @@
+
 const TZFEToken = artifacts.require("./TZFEToken.sol")
 
 contract("TZFEToken", accounts=> {
@@ -9,12 +10,32 @@ contract("TZFEToken", accounts=> {
 
   describe('deployment', async () => {
     it('deploys successfully', async () => {
-      console.log(tzfeToken)
+      //console.log(tzfeToken)
       const address = await tzfeToken.address
       assert.notEqual(address, 0x0)
       assert.notEqual(address, '')
       assert.notEqual(address, null)
       assert.notEqual(address, undefined)
+    })
+  })
+  describe('token deployed', async () => {
+    it('owner gets 1000 token', async () => {
+      const owner = await tzfeToken.owner();
+      const balanceObj = await tzfeToken.balanceOf(owner);
+      const balance = balanceObj.words[0];
+      assert.equal(balance, 1000)
+
+    })
+  })
+  describe('reward token deployed', async () => {
+    it('player gets 90 token as reward', async () => {
+      const player = '0x969E5682346759E90084D3BeB772E37eDb9B5Fc7';
+      const originalBalanceObj = await tzfeToken.balanceOf(player);
+      const originalBalance = originalBalanceObj.words[0];
+      await tzfeToken.reward(player, 90);
+      const newBalanceObj = await tzfeToken.balanceOf(player);
+      const newBalance = newBalanceObj.words[0];
+      assert.equal(newBalance - originalBalance, 90)
     })
   })
 })
