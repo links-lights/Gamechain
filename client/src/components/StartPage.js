7e3cc9f2cf630d303fe340f0d579cae4619637ae
Freddy Gomez <fjohngomez90@gmail.com>
2021-10-12 17:07:10 -0400

    Added Game to startPage

--- client/src/components/StartPage.js
+++ client/src/components/StartPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Game from './Game'
 import { Paper, Grid } from "@mui/material"
 
 class StartPage extends React.Component {
@@ -6,9 +7,10 @@ class StartPage extends React.Component {
   //score
   //methods for score bind this
   render () {
+    console.log(this.props)
     return (
       <Paper sx={{
-        height:"50vw"
+        minHeight:"50vw"
       }}>
         <Grid container direction="column" className="sections">
           <Grid item
@@ -17,13 +19,13 @@ class StartPage extends React.Component {
             height:"5vw",
             border:"1px solid black"
           }} />
-          <Grid item container className="GameArea" sx={{border:"1px solid black", height:"30vw"}}>
+          <Grid item container className="GameArea" sx={{border:"1px solid black", minHeight:"30vw"}}>
           <Grid item xs={2}>
           Description  here
           </Grid>
           <Grid item xs={8}
           sx={{border:"1px solid black"}}>
-            Game component here
+            <Game drizzle={this.props.drizzle} drizzleState={this.props.drizzleState} />
           </Grid>
           <Grid item xs={2}>
             Score here
