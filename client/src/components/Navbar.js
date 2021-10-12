70841ce54cbc67397cc8ff3bf20485bd6a698177
emil <poolsar.42@gmail.com>
2021-10-12 22:00:14 +0300

    feat: adds styles for navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -8,18 +8,33 @@ const Navbar = (props) => {
   return (
     <div className="navbar">
       <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
+        <AppBar position="static" sx={{ "background-color": "#BD431D" }}>
           <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 10 }}>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 10 }}
+              className="link"
+            >
               <Link to="/"> Home</Link>
             </Typography>
             <Button variant="outlined">
-              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <Typography
+                variant="h6"
+                component="div"
+                sx={{ flexGrow: 1 }}
+                className="link"
+              >
                 <Link to="/game"> Game</Link>
               </Typography>
             </Button>
             <Button variant="outlined">
-              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <Typography
+                variant="h6"
+                component="div"
+                sx={{ flexGrow: 1 }}
+                className="link"
+              >
                 <Link to="/"> My Account</Link>
               </Typography>
             </Button>
