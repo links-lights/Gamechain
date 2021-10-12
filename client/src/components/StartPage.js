bb3d1203e340dc90f4d97797d2a5f97686fe1f00
Freddy Gomez <fjohngomez90@gmail.com>
2021-10-12 16:55:14 -0400

    Merged navBar-edit with startpage component

--- client/src/components/StartPage.js
+++ client/src/components/StartPage.js
@@ -2,6 +2,9 @@ import React from "react";
 import { Paper, Grid } from "@mui/material"
 
 class StartPage extends React.Component {
+
+  //score
+  //methods for score bind this
   render () {
     return (
       <Paper sx={{
@@ -15,12 +18,16 @@ class StartPage extends React.Component {
             border:"1px solid black"
           }} />
           <Grid item container className="GameArea" sx={{border:"1px solid black", height:"30vw"}}>
-          <Grid item xs={1} />
-          <Grid item xs={10}
+          <Grid item xs={2}>
+          Description  here
+          </Grid>
+          <Grid item xs={8}
           sx={{border:"1px solid black"}}>
             Game component here
           </Grid>
-          <Grid item xs={1} />
+          <Grid item xs={2}>
+            Score here
+            </Grid>
           </Grid>
           <Grid item className="footer" sx={{border:"1px solid black"}}>
             Footer here
