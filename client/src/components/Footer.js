7e244bf7ac33e71a05d3a10a77bebe882444faef
emil <poolsar.42@gmail.com>
2021-10-13 19:47:15 +0300

    feat: footer plain css

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,46 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Box, Toolbar, Button, AppBar, Typography } from "@mui/material";
-
 import "../styles/Footer.css";
 
 const Footer = (props) => {
   return (
     <div className="footer">
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static" sx={{ backgroundColor: "#BD431D" }}>
-          <Toolbar>
-            <Typography
-              variant="h6"
-              component="div"
-              sx={{ flexGrow: 10 }}
-              className="link"
-            >
-              <Link to="/"> Home</Link>
-            </Typography>
-            <Button variant="outlined">
-              <Typography
-                variant="h6"
-                component="div"
-                sx={{ flexGrow: 1 }}
-                className="link"
-              >
-                <Link to="/game"> Game</Link>
-              </Typography>
-            </Button>
-            <Button variant="outlined">
-              <Typography
-                variant="h6"
-                component="div"
-                sx={{ flexGrow: 1 }}
-                className="link"
-              >
-                <Link to="/"> My Account</Link>
-              </Typography>
-            </Button>
-          </Toolbar>
-        </AppBar>
-      </Box>
+      <h2 className="item">
+        <a href="https://github.com/links-lights/Gamechain/projects/1">
+          <i class="fa fa-github" aria-hidden="true"></i>
+        </a>
+      </h2>
+      <h2 className="item">
+        <a href="https://github.com/links-lights/Gamechain/projects/1">
+          Game Source Code
+        </a>
+      </h2>
     </div>
   );
 };
