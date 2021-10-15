75485021c3a3dfd9cc2668dc954fe88745e2a783
Ben Ye <oceanblueye@gmail.com>
2021-10-15 17:18:38 -0400

    revised NFToken contract and added DeployContract

--- contracts/NFToken.sol
+++ contracts/NFToken.sol
@@ -5,19 +5,20 @@ import "../client/node_modules/@openzeppelin/contracts/token/ERC1155/ERC1155.sol
 import "../client/node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
 contract GameNFT is ERC1155, Ownable {
-    uint256 public constant regular = 0;
-    uint256 public constant rare = 1;
-    uint256 public constant superRare = 2;
+    uint256 public constant OakTree = 0;
+    uint256 public constant PineTree = 1;
+    uint256 public constant WeepingWillow = 2;
+    uint256 public constant BonsaiTree = 3;
+    uint256 public constant CherryTree = 4;
 
 // need to pass in an URI addrees from IPFS behind ERC1155 later not sure? but it works fine as it is right now
-    constructor() ERC1155("GNFT") {
-       _mint(msg.sender, regular, 9000, "");
-       _mint(msg.sender, rare, 900, "");
-       _mint(msg.sender, superRare, 90, "");
+    constructor() ERC1155("https://ipfs.io/ipfs/QmWgVfbWDHHvLm9DSy6XV4ug97ah1XNKhCBFksr3ZbqJAf/{id}.json") {
+       _mint(msg.sender, WeepingWillow, 1, "");
+       _mint(msg.sender, OakTree, 1, "");
+       _mint(msg.sender, PineTree, 1, "");
+       _mint(msg.sender, BonsaiTree, 1, "");
+       _mint(msg.sender, CherryTree, 1, "");
     }
     
-     function reward(address player, uint256 amount) public {
-        require (amount > 0);
-        _mint(player, regular, amount, "");
-    }
+   
 }
