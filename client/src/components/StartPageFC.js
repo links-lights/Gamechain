dd0df04ed0aa5d6e35291f7185c3e9d8ff150e12
Ben Ye <oceanblueye@gmail.com>
2021-10-19 17:58:45 -0400

    edited package.jon and config.js and gitignore to prepare to deploy to Ropsten testnet

--- client/src/components/StartPageFC.js
+++ client/src/components/StartPageFC.js
@@ -5,142 +5,134 @@ import { Paper, Grid } from "@mui/material";
 import RecipeReviewCard from "./GameDescripion";
 import { changeUser, fetchUser, createUser } from "../db/models/user";
 
-function StartPage (props) {
-    const [score, setScore] = useState(0);
-    const [highScore, setHighScore] = useState(0);
-    const [rewardAmount, setRewardAmount] = useState(0);
-    const [board, setBoard] = useState();
-    const [user, setUser] = useState();
+function StartPage(props) {
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+  const [rewardAmount, setRewardAmount] = useState(0);
+  const [board, setBoard] = useState([]);
+  const [user, setUser] = useState();
 
-    useEffect(()=> {
-        (async () => {
-        const account = props.drizzleState.accounts[0];
-        let _user = (await fetchUser(account))[0];
-        if (!_user) {
-          _user = (
-            await createUser(
-              account,
-              account,
-              "QmXiYAbTQP4yMbjbNVJc4NyPskY88gwXqSoMPBPHrarGTe",
-              0
-            )
-          )[0];
-        }
-        setUser(_user)
-        setHighScore(_user.score);
-
-    })()}
-    ,[highScore, props.drizzleState.accounts])
-
-    async function awardAmount(amount) {
-        await setRewardAmount(amount);
-      }
-
-    async function postHighScore() {
-        const account = props.drizzleState.accounts[0];
-        if (score > highScore) {
-          setHighScore(score);
-          await changeUser(
+  useEffect(() => {
+    (async () => {
+      const account = props.drizzleState.accounts[0];
+      let _user = (await fetchUser(account))[0];
+      if (!_user) {
+        _user = (
+          await createUser(
             account,
-            user.username,
-            user.imageHash,
-            score
-          );
-        }
+            account,
+            "QmXiYAbTQP4yMbjbNVJc4NyPskY88gwXqSoMPBPHrarGTe",
+            0
+          )
+        )[0];
       }
+      setUser(_user);
+      setHighScore(_user.score);
+    })();
+  }, [highScore, props.drizzleState.accounts]);
 
-      async function setReward() {
-        let highestBoard = 0;
-        const contract = props.drizzle.contracts.TZFEToken;
-        const account = props.drizzleState.accounts[0];
-        let amount = 0;
-        //console.log('board', board)
-        board.forEach((row) => {
-          highestBoard = Math.max(...row, highestBoard);
-        });
-        if (highestBoard >= 4) {
-          amount++;
-        }
-        if (score >= 100) {
-          amount++;
-        }
-        if (highestBoard >= 8) {
-          amount++;
-        }
-        if (score >= 200) {
-          amount++;
-        }
-        if (highestBoard >= 2048) {
-          amount++;
-        }
-        if (score >= 20000) {
-          amount++;
-        }
-        console.log("amount", amount);
-        if (amount > 0) {
-          await contract.methods.reward(account, amount).send({ from: account });
-          //but why?
-          // await this.awardAmount(amount);
-          // I think we can do this insted - please correct me if I'm mistakern
-          await awardAmount(amount + rewardAmount);
-        }
-    
-        postHighScore();
-    
-        console.log(await contract.methods.balanceOf(account).call());
-      }
+  async function awardAmount(amount) {
+    await setRewardAmount(amount);
+  }
 
+  async function postHighScore() {
+    const account = props.drizzleState.accounts[0];
+    if (score > highScore) {
+      setHighScore(score);
+      await changeUser(account, user.username, user.imageHash, score);
+    }
+  }
 
-    return (
-        <Paper
-        sx={{
-          minHeight: "50vw",
-        }}
-      >
-        <Grid container direction="column" className="sections">
-          <Grid
-            item
-            className="topSpacer"
-            sx={{
-              height: "5vw",
-              border: "1px solid black",
-            }}
-          />
-          <Grid
-            item
-            container
-            className="GameArea"
-            sx={{ border: "1px solid black", minHeight: "30vw" }}
-          >
-            <Grid item xs={2}>
-              <RecipeReviewCard />
-            </Grid>
-            <Grid item xs={8} sx={{ border: "1px solid black" }}>
-              <Game
-                drizzle={props.drizzle}
-                drizzleState={props.drizzleState}
-                awardAmount={awardAmount}
-                highScore={postHighScore}
-                setReward={setReward}
-                setScore={setScore}
-                setBoard={setBoard}
-              />
-            </Grid>
-            <Grid item xs={2}>
-              <TokenAward
-                drizzle={props.drizzle}
-                drizzleState={props.drizzleState}
-                highScore={highScore}
-                rewardAmount={rewardAmount}
-              />
-            </Grid>
+  async function setReward() {
+    let highestBoard = 0;
+    const contract = props.drizzle.contracts.TZFEToken;
+    const account = props.drizzleState.accounts[0];
+    let amount = 0;
+    board.forEach((row) => {
+      highestBoard = Math.max(...row, highestBoard);
+    });
+    if (highestBoard >= 4) {
+      amount++;
+    }
+    if (score >= 100) {
+      amount++;
+    }
+    if (highestBoard >= 8) {
+      amount++;
+    }
+    if (score >= 200) {
+      amount++;
+    }
+    if (highestBoard >= 2048) {
+      amount++;
+    }
+    if (score >= 20000) {
+      amount++;
+    }
+    console.log("amount", amount);
+    if (amount > 0) {
+      await contract.methods.reward(account, amount).send({ from: account });
+      //but why?
+      // await this.awardAmount(amount);
+      // I think we can do this insted - please correct me if I'm mistakern
+      await awardAmount(amount + rewardAmount);
+    }
+
+    postHighScore();
+
+    console.log(await contract.methods.balanceOf(account).call());
+  }
+
+  return (
+    <Paper
+      sx={{
+        minHeight: "50vw",
+      }}
+    >
+      <Grid container direction="column" className="sections">
+        <Grid
+          item
+          className="topSpacer"
+          sx={{
+            height: "5vw",
+            border: "1px solid black",
+          }}
+        />
+        <Grid
+          item
+          container
+          className="GameArea"
+          sx={{ border: "1px solid black", minHeight: "30vw" }}
+        >
+          <Grid item xs={2}>
+            <RecipeReviewCard />
           </Grid>
-          <Grid item className="footer" sx={{ border: "1px solid black" }}>
-            Footer here
+          <Grid item xs={8} sx={{ border: "1px solid black" }}>
+            <Game
+              drizzle={props.drizzle}
+              drizzleState={props.drizzleState}
+              awardAmount={awardAmount}
+              highScore={postHighScore}
+              setReward={setReward}
+              setScore={setScore}
+              setBoard={setBoard}
+            />
+          </Grid>
+          <Grid item xs={2}>
+            <TokenAward
+              drizzle={props.drizzle}
+              drizzleState={props.drizzleState}
+              highScore={highScore}
+              rewardAmount={rewardAmount}
+            />
           </Grid>
         </Grid>
-      </Paper>
-    )
+        <Grid item className="footer" sx={{ border: "1px solid black" }}>
+          Footer here
+        </Grid>
+      </Grid>
+    </Paper>
+  );
 }
 
-export default StartPage
+export default StartPage;
