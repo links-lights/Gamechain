9915812c3768426239b2a4fae3b5584c2f0af336
emil <poolsar.42@gmail.com>
2021-10-22 18:59:36 +0300

    test: nft

--- contracts/NFToken.sol
+++ contracts/NFToken.sol
@@ -1,6 +1,5 @@
 pragma solidity ^0.8.0;
 
-
 import "../client/node_modules/@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "../client/node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
@@ -11,14 +10,15 @@ contract GameNFT is ERC1155, Ownable {
     uint256 public constant BonsaiTree = 3;
     uint256 public constant CherryTree = 4;
 
-// need to pass in an URI addrees from IPFS behind ERC1155 later not sure? but it works fine as it is right now
-    constructor() ERC1155("https://ipfs.io/ipfs/QmWgVfbWDHHvLm9DSy6XV4ug97ah1XNKhCBFksr3ZbqJAf/{id}.json") {
-       _mint(msg.sender, WeepingWillow, 1, "");
-       _mint(msg.sender, OakTree, 1, "");
-       _mint(msg.sender, PineTree, 1, "");
-       _mint(msg.sender, BonsaiTree, 1, "");
-       _mint(msg.sender, CherryTree, 1, "");
+    // need to pass in an URI addrees from IPFS behind ERC1155 later not sure? but it works fine as it is right now
+    constructor()
+        ERC1155(
+            "https://ipfs.io/ipfs/QmWgVfbWDHHvLm9DSy6XV4ug97ah1XNKhCBFksr3ZbqJAf/{id}.json"
+        )
+    {}
+
+    function reward(address _user, uint256 _tokenId) public {
+        require(_tokenId >= 0 && _tokenId < 5);
+        _mint(_user, _tokenId, 1, "");
     }
-    
-   
 }
