5d78b234c1aca1671278f677d8705bf2a4b041e2
emil <poolsar.42@gmail.com>
2021-10-23 23:33:06 +0300

    feat: adds start for three.js

--- client/src/components/Three.js
+++ client/src/components/Three.js
@@ -1,33 +1,95 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
 // SASS extension - SASS for CSS like TypeScript for JavaScript
 import "../styles/Three.scss";
 
 import { Canvas, useFrame } from "@react-three/fiber";
 
-// import { Box } from "@react-three/drei";
+import {
+  softShadows,
+  MeshWobbleMaterial,
+  OrbitControls,
+} from "@react-three/drei";
 
-const SpinningMesh = (props) => {
+import { useSpring, a } from "@react-spring/three";
+
+softShadows();
+
+const SpinningMesh = ({ position, args, color, speed }) => {
   const mesh = useRef(null);
   useFrame(() => {
     mesh.current.rotation.x = mesh.current.rotation.y += 0.01;
   });
+
+  const [expand, setExpland] = useState(false);
+
+  const props = useSpring({
+    scale: expand ? [1.4, 1.4, 1.4] : [1, 1, 1],
+  });
+
   return (
-    <mesh {...props} ref={mesh}>
-      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshStandardMaterial attach="material" color="hotpink" />
-    </mesh>
+    <a.mesh
+      onClick={() => setExpland(!expand)}
+      scale={props.scale}
+      castShadow
+      position={position}
+      ref={mesh}
+    >
+      <boxBufferGeometry castShadow attach="geometry" args={args} />
+      <MeshWobbleMaterial
+        castShadow
+        attach="material"
+        color={color}
+        speed={speed}
+        fac={0.6}
+      />
+    </a.mesh>
   );
 };
 
 function Three(props) {
   return (
     <>
-      <Canvas colorManagement camera={{ position: [-5, 2, 10], fov: 60 }}>
+      {/* shadows instead of shadowMap */}
+      <Canvas shadows camera={{ position: [-5, 2, 10], fov: 60 }}>
         <ambientLight intensity={0.3} />
-        <SpinningMesh position={[0, 1, 0]} />
-        <SpinningMesh position={[-2, 1, -5]} />
-        <SpinningMesh position={[5, 1, -2]} />
+        <directionalLight
+          castShadow
+          position={[0, 10, 0]}
+          intensity={1.5}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-far={50}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={-10}
+        />
+        <pointLight position={[-10, 0, -20]} intensity={0.5} />
+        <pointLight position={[0, -10, 0]} intensity={1.5} />
+
+        {/* This is our floor */}
+        <group>
+          <mesh
+            receiveShadow
+            rotation={[-Math.PI / 2, 0, 0]}
+            position={[0, -3, 0]}
+          >
+            <planeBufferGeometry attach="geometry" args={[100, 100]} />
+            <shadowMaterial attach="material" opacity={0.3} />
+          </mesh>
+
+          <SpinningMesh
+            position={[0, 1, 0]}
+            args={[3, 2, 1]}
+            color="hotpink"
+            speed={2}
+          />
+          <SpinningMesh position={[-2, 1, -5]} color="lightblue" speed={6} />
+          <SpinningMesh position={[5, 1, -2]} color="lightblue" speed={6} />
+        </group>
+
+        <OrbitControls />
       </Canvas>
     </>
   );
