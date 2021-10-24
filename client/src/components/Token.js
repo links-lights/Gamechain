488ff610de5e51305aa392792a5ee06ad8fec6c6
emil <poolsar.42@gmail.com>
2021-10-25 02:34:29 +0300

    feat: change floor color

--- client/src/components/Token.js
+++ client/src/components/Token.js
@@ -62,8 +62,9 @@ function Token(props) {
             rotation={[-Math.PI / 2, 0, 0]}
             position={[0, -3, 0]}
           >
-            <planeBufferGeometry attach="geometry" args={[100, 100]} />
+            <planeBufferGeometry attach="geometry" args={[1000, 1000]} />
             <shadowMaterial attach="material" opacity={0.3} />
+            <meshStandardMaterial attach="material" color="blue" />
           </mesh>
 
           <SpinningMesh
