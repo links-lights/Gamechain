2ece811c8e32f0e21f7f57d61c50ba6358b6d7cc
Freddy Gomez <fjohngomez90@gmail.com>
2021-10-26 16:45:14 -0400

    MUI update to Scoreboard

--- client/src/components/Scoreboard.js
+++ client/src/components/Scoreboard.js
@@ -2,6 +2,7 @@ import { fetchUsers } from "../db/models/user";
 import React, { useState, useEffect } from "react";
 
 import "../styles/Scoreboard.css";
+import { Paper, Box, Typography, Divider, TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Grid} from "@mui/material";
 
 const Scoreboard = (props) => {
   const [users, setUsers] = useState([]);
@@ -17,39 +18,103 @@ const Scoreboard = (props) => {
   }, []);
 
   return (
-    <>
-      <h1>Scoreboard</h1>
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th></th>
-            <th>
-              <h3>Username</h3>
-            </th>
-            <th>
-              <h3>High Score</h3>
-            </th>
-          </tr>
-          {users.map((user, key) => {
+    <Box gridTemplateColumns="repeat(12, 1fr)" p={4}  gap={2}
+    sx={{
+      minHeight:"48vw"
+    }}>
+      <Box className="title"  gridColumn="span 12"
+      sx={{
+        textAlign:"center"
+      }}>
+        <Typography variant="h3">
+          ScoreBoard
+          <br></br>
+          <Divider />
+        </Typography>
+      </Box>
+      <Grid container p={10}>
+        <Grid item xs={3} />
+      <Grid item className="scoreboard" xs={6}>
+        <TableContainer component={Paper}>
+          <Table sx={{
+            minHeight: "20vw",
+            }}>
+            <TableHead>
+              <TableRow>
+                <TableCell>
+                  Rank
+                </TableCell>
+                <TableCell>
+                  {``}
+                  </TableCell>
+                <TableCell>
+                  Username
+                </TableCell>
+                <TableCell align="right">
+                  HighScore
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+
+            {users.map((user, key) => {
             return (
-              <tr key={key}>
-                <th>{key + 1}</th>
-                <th>
+              <TableRow key={key}>
+                <TableCell component="th">{key + 1}</TableCell>
+                <TableCell>
                   <img
                     src={`https://ipfs.io/ipfs/${user.imageHash}`}
                     alt="user pfp"
                     className="scoreboard-pic"
                   />
-                </th>
-                <th>{user.username}</th>
-                <th>{user.score}</th>
-              </tr>
+                </TableCell>
+                <TableCell>{user.username}</TableCell>
+                <TableCell align="right">{user.score}</TableCell>
+              </TableRow>
             );
           })}
-        </tbody>
-      </table>
-    </>
+
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
+      <Grid item xs={3} />
+      </Grid>
+    </Box>
+
+    // <>
+    //   <h1>Scoreboard</h1>
+    //   <table>
+    //     <tbody>
+    //       <tr>
+    //         <th></th>
+    //         <th></th>
+    //         <th>
+    //           <h3>Username</h3>
+    //         </th>
+    //         <th>
+    //           <h3>High Score</h3>
+    //         </th>
+    //       </tr>
+          // {users.map((user, key) => {
+          //   return (
+          //     <tr key={key}>
+          //       <th>{key + 1}</th>
+          //       <th>
+          //         <img
+          //           src={`https://ipfs.io/ipfs/${user.imageHash}`}
+          //           alt="user pfp"
+          //           className="scoreboard-pic"
+          //         />
+          //       </th>
+          //       <th>{user.username}</th>
+          //       <th>{user.score}</th>
+          //     </tr>
+          //   );
+          // })}
+    //     </tbody>
+    //   </table>
+    // </>
   );
 };
 
