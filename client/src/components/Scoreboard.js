67a237ba05bacdfb13d4c1f5fc1e84ac20616665
Freddy Gomez <fjohngomez90@gmail.com>
2021-10-26 17:30:17 -0400

    cleaned up workspace

--- client/src/components/Scoreboard.js
+++ client/src/components/Scoreboard.js
@@ -123,40 +123,6 @@ const Scoreboard = (props) => {
         </Grid>
       </Grid>
     </Box>
-
-    // <>
-    //   <h1>Scoreboard</h1>
-    //   <table>
-    //     <tbody>
-    //       <tr>
-    //         <th></th>
-    //         <th></th>
-    //         <th>
-    //           <h3>Username</h3>
-    //         </th>
-    //         <th>
-    //           <h3>High Score</h3>
-    //         </th>
-    //       </tr>
-          // {users.map((user, key) => {
-          //   return (
-          //     <tr key={key}>
-          //       <th>{key + 1}</th>
-          //       <th>
-          //         <img
-          //           src={`https://ipfs.io/ipfs/${user.imageHash}`}
-          //           alt="user pfp"
-          //           className="scoreboard-pic"
-          //         />
-          //       </th>
-          //       <th>{user.username}</th>
-          //       <th>{user.score}</th>
-          //     </tr>
-          //   );
-          // })}
-    //     </tbody>
-    //   </table>
-    // </>
   );
 };
 
