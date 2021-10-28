9ba3d383f3a298ccf093ada6965f750ec8036471
Freddy Gomez <fjohngomez90@gmail.com>
2021-10-28 17:43:54 -0400

    Applied MUI to odds tables

--- client/src/components/TokenOdds.js
+++ client/src/components/TokenOdds.js
@@ -1,38 +1,102 @@
+import { Table, TableContainer, TableHead, TableBody, TableRow, TableCell, Typography } from "@mui/material"
+
 function TokenOddsTable(){
+  const tileScores =[ {score: 512, percentage:10}, {score: 1024, percentage: 15}, {score:2048, percentage:15}]
+  const scores = [ {score: 5000, percentage: 10 }, {score: 10000, percentage:15 }, {score: 20000, percentage:15}]
   return (
-  <table>
-    <th>
-     Token Odds
-    </th>
-     <tr>
-       <th>Milestone</th>
-       <th>Odds increased</th>
-     </tr>
-     <tr>
-       <td>512 Tile Score </td>
-       <td>+10%</td>
-     </tr>
-     <tr>
-       <td>5000 Total Score </td>
-       <td>+10%</td>
-     </tr>
-     <tr>
-       <td>1024 Tile Score </td>
-       <td>+15%</td>
-     </tr>
-     <tr>
-       <td>10000 Total Score </td>
-       <td>+15%</td>
-     </tr>
-     <tr>
-       <td>2048 Tile Score </td>
-       <td>+15%</td>
-     </tr>
-     <tr>
-       <td>20000 Total Score </td>
-       <td>+15% + Can earn multiple tokens</td>
-     </tr>
-   </table>
+    <>
+    <Typography variant="h5">
+      Token Odds
+    </Typography>
+    <TableContainer>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell sx={{fontWeight:"bold"}}>
+             Tile
+            </TableCell>
+            <TableCell align="right" sx={{fontWeight:"bold"}}>
+              Odds Increased
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {tileScores.map((odds, idx)=>{
+            return (
+              <TableRow key={idx}>
+                <TableCell>
+                  {odds.score}
+                </TableCell>
+                <TableCell align="right">
+                  {`+${odds.percentage}%`}
+                </TableCell>
+              </TableRow>
+            )
+          })}
+        </TableBody>
+      </Table>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell sx={{fontWeight:"bold"}}>
+             Score
+            </TableCell>
+            <TableCell align="right" sx={{fontWeight:"bold"}}>
+              Odds Increased
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {scores.map((odds, idx)=>{
+            return (
+              <TableRow key={idx}>
+                <TableCell>
+                  {odds.score}
+                </TableCell>
+                <TableCell align="right">
+                  {`+${odds.percentage}%`}
+                </TableCell>
+              </TableRow>
+            )
+          })}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    </>
+
+  // <table>
+  //   <th>
+  //    Token Odds
+  //   </th>
+  //    <tr>
+  //      <th>Milestone</th>
+  //      <th>Odds increased</th>
+  //    </tr>
+  //    <tr>
+  //      <td>512 Tile Score </td>
+  //      <td>+10%</td>
+  //    </tr>
+  //    <tr>
+  //      <td>5000 Total Score </td>
+  //      <td>+10%</td>
+  //    </tr>
+  //    <tr>
+  //      <td>1024 Tile Score </td>
+  //      <td>+15%</td>
+  //    </tr>
+  //    <tr>
+  //      <td>10000 Total Score </td>
+  //      <td>+15%</td>
+  //    </tr>
+  //    <tr>
+  //      <td>2048 Tile Score </td>
+  //      <td>+15%</td>
+  //    </tr>
+  //    <tr>
+  //      <td>20000 Total Score </td>
+  //      <td>+15% + Can earn multiple tokens</td>
+  //    </tr>
+  //  </table>
    )
 }
 export default TokenOddsTable
