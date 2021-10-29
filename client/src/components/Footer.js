0f8c823654cd7f5d394713107097508f74e5e648
Freddy Gomez <fjohngomez90@gmail.com>
2021-10-29 14:20:58 -0400

    about us section added to footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,19 +1,25 @@
+import { AppBar, Toolbar } from "@mui/material";
+import { Link } from "react-router-dom"
 import React from "react";
 import "../styles/Footer.css";
 
 const Footer = (props) => {
   return (
+
     <div className="footer">
       <h2 className="item">
         <a href="https://github.com/links-lights/Gamechain/projects/1">
           <i className="fa fa-github" aria-hidden="true"></i>
         </a>
       </h2>
-      <h2 className="item">
+      <h3 className="item">
         <a href="https://codepen.io/jeffleu/pen/JRzyPZ">
           Game Source Code
         </a>
-      </h2>
+      </h3>
+      <h3 classname="item">
+        <Link to="/aboutus">About us</Link>
+      </h3>
     </div>
   );
 };
